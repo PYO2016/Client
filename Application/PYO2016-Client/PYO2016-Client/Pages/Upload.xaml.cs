f24e5d99db61f15e6123889df881eea48cbb3a54
vyujing <vyujing@gmail.com>
2015-11-23 01:10:14 +0900

    capture tool job finish

--- Application/PYO2016-Client/PYO2016-Client/Pages/Upload.xaml.cs
+++ Application/PYO2016-Client/PYO2016-Client/Pages/Upload.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PYO2016_Client.Sources.Capture;
 
 namespace PYO2016_Client.Pages
 {
@@ -28,6 +29,10 @@ namespace PYO2016_Client.Pages
         private void ListViewItem_Selected(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void captureButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            CaptureTool.getInstance().capture("C:\\Users\\KGWANGMIN\\Documents");
         }
     }
 }
--- Application/PYO2016-Client/PYO2016-Client/Sources/Capture/CaptureTool.cs
+++ Application/PYO2016-Client/PYO2016-Client/Sources/Capture/CaptureTool.cs
@@ -10,16 +10,27 @@ using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System.Windows.Interop;
+using System.Windows.Input;
+using System.IO;
 
 namespace PYO2016_Client.Sources.Capture
 {
     class CaptureTool
     {
         private static CaptureTool instance = null;
+        // is clicking??
+        private bool isInClick;
+        private System.Windows.Point clickPoint;
         private static Window mainWindow;
+        private Canvas canvas;
+        private String path;
+        private BitmapSource bitmapSource;
+        System.Windows.Shapes.Rectangle rect;
 
         private CaptureTool()
-        {}
+        {
+            isInClick = false;
+        }
         public static CaptureTool getInstance()
         {
             if (instance == null)
@@ -30,27 +41,27 @@ namespace PYO2016_Client.Sources.Capture
         public void capture(String path)
         {
             mainWindow = new Window();
-            
+            this.path = path;
             Rectangle resolution = Screen.PrimaryScreen.Bounds;
             
             mainWindow.WindowState = WindowState.Maximized;
             mainWindow.WindowStyle = WindowStyle.None;
             mainWindow.BorderThickness = new Thickness(0);
 
-            Canvas myCanvas = new Canvas();
-            myCanvas.Width = resolution.Width;
-            myCanvas.Height = resolution.Height; 
-            myCanvas.Background = getImageBrush();
+            canvas = new Canvas();
+            canvas.Width = resolution.Width;
+            canvas.Height = resolution.Height; 
+            canvas.Background = getImageBrush();
             
-            mainWindow.Content = myCanvas;
+            mainWindow.Content = canvas;
             mainWindow.MouseDown += new System.Windows.Input.MouseButtonEventHandler(mouseDown);
+            mainWindow.MouseMove += new System.Windows.Input.MouseEventHandler(mouseMove);
+            mainWindow.MouseUp += new System.Windows.Input.MouseButtonEventHandler(mouseDown);
             mainWindow.Show();
         }
 
         private ImageBrush getImageBrush()
         {
-            System.Drawing.Image img = System.Windows.Forms.Clipboard.GetImage();
-
             Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             Graphics graphics = Graphics.FromImage(printscreen as System.Drawing.Image);
 
@@ -58,7 +69,7 @@ namespace PYO2016_Client.Sources.Capture
             
 
             //var bitmap = new System.Drawing.Bitmap(img);
-            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(printscreen.GetHbitmap(),
+            bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(printscreen.GetHbitmap(),
                                                                         IntPtr.Zero,
                                                                         Int32Rect.Empty,
                                                                         BitmapSizeOptions.FromEmptyOptions()
@@ -68,9 +79,84 @@ namespace PYO2016_Client.Sources.Capture
             return brush;
         }
 
-        private void mouseDown(object sender, EventArgs e)
+        private void mouseDown(object sender, MouseButtonEventArgs e)
         {
-            System.Windows.MessageBox.Show("Event Catch");
+            //System.Windows.MessageBox.Show("Event Catch");
+            if (isInClick == false) { 
+                isInClick = true;
+                clickPoint = e.GetPosition(canvas);
+                rect = new System.Windows.Shapes.Rectangle();
+                rect.Stroke = new SolidColorBrush(Colors.Aqua);
+                rect.Fill = new SolidColorBrush(Colors.Aqua);
+                rect.Opacity = 0.5;
+                rect.Width = 0;
+                rect.Height = 0;
+                Canvas.SetLeft(rect, this.clickPoint.X);
+                Canvas.SetTop(rect, this.clickPoint.Y);
+                canvas.Children.Add(rect);
+            }
+        }
+        private void mouseMove(object sender, EventArgs e)
+        {
+            if(Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                System.Windows.Point p = Mouse.GetPosition(canvas);
+                rect.Width = Math.Abs(this.clickPoint.X - p.X);
+                rect.Height = Math.Abs(this.clickPoint.Y - p.Y);
+                if (this.clickPoint.X - p.X > 0)
+                    Canvas.SetLeft(rect, this.clickPoint.X - Math.Abs(this.clickPoint.X - p.X));
+                else
+                    Canvas.SetLeft(rect, this.clickPoint.X);
+                if (this.clickPoint.Y - p.Y > 0)
+                    Canvas.SetTop(rect, this.clickPoint.Y - Math.Abs(this.clickPoint.Y - p.Y));
+                else
+                    Canvas.SetTop(rect, this.clickPoint.Y);
+            }
+            else if (Mouse.LeftButton == MouseButtonState.Released)
+            {
+                if (isInClick)
+                {
+                    System.Windows.Point p = Mouse.GetPosition(canvas);
+                    isInClick = false;
+                    canvas.Children.Remove(rect);
+                    mainWindow.Hide();
+                    double x, y;
+                    if (this.clickPoint.X - p.X > 0)
+                        x = this.clickPoint.X - Math.Abs(this.clickPoint.X - p.X);
+                    else
+                        x = this.clickPoint.X;
+                    if (this.clickPoint.Y - p.Y > 0)
+                        y = this.clickPoint.Y - Math.Abs(this.clickPoint.Y - p.Y);
+                    else
+                        y = this.clickPoint.Y;
+
+                    SaveBitmapSourceImageToFile(x, y
+                        , Math.Abs(this.clickPoint.X - p.X)
+                        , Math.Abs(this.clickPoint.Y - p.Y));
+
+                    mainWindow.Close();
+                }
+            }
+        }
+        public void SaveBitmapSourceImageToFile(double x1, double y1, double x2, double y2)
+        {
+            var image = this.bitmapSource;
+            var cI = new CroppedBitmap(image, new Int32Rect((int)x1, (int)y1, (int)x2, (int)y2));
+
+            this.path += "\\" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(cI));
+                encoder.Save(fileStream);
+            }
+        }
+
+        private void mouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //System.Windows.MessageBox.Show("Event Catch");
+            if (isInClick)
+                isInClick = false;
         }
 
     }
